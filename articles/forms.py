c09a5333a2e7fc1c649a716c29fa9142c1236624
Syed Dayim Shah <90561337+syedDayim@users.noreply.github.com>
2022-04-02 13:45:40 +0530

    Add files via upload

---
+++ articles/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+from .import models
+
+class CreateArticle(forms.ModelForm):
+    class Meta:
+        model = models.Articles
+        fields = [
+            'title',
+            'body',
+            'slug',
+            'thumb',
+        ]
